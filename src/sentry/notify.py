62c8a650d67a34d68e5d51ea93034fc3dfc9632d
Patrick M. Covert <pmcovert@buffaloist.com>
2020-06-21 07:17:27 -0400

    notify module works, but is not active in the package

--- src/sentry/notify.py
+++ src/sentry/notify.py
@@ -1,7 +1,18 @@
-import subprocess
+import os
+def notify():
+    f = open('/tmp/pingmsg.txt', 'w')
+    f.write("""\
+            Greetings,
+            
+            I have detected a server error!  Please check the Sentry Log on 192.168.1.24.
+            
+            - Sentry
+            """)
+    f.close()
+	
+    os.system("mutt -s 'Ping Check' pmcovert@buffaloist.com < /tmp/pingmsg.txt")
+    
+    
+    
 
-subprocess.Popen(["echo 'Sentry detects a network failure! Check Sentry Logs!' > /tmp/pingmsg.txt"])
-process = subprocess.Popen(["mutt -s 'Ping Check' pmcovert@buffaloist.com < /tmp/pingmsg.txt"], stdout=subprocess.PIPE)
-output = process.communicate()
-output = str(output[0])
-return output
+
