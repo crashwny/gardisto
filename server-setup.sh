2bf9afa92a4b3fdeb1eaba6d3edeb422b059fda6
Patrick Covert <crashwny@gmail.com>
2021-02-07 19:59:48 -0500

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -24,14 +24,24 @@ touch /var/gardisto/notify.log
 chown -R gardisto:gardisto /var/gardisto
 
 if [ $os_type = "apt" ]; then
-	apt install git
+	apt install git -y
+	apt install zip -y
 else
-	yum install git
+	yum install git -y
+	yum install zip -y
+fi
 
-su gardisto "cd /home/gardisto/ ;git clone http://173.64.3.56:65524/gitlab/patrick/gardisto.git"
+cd /tmp
+wget https://github.com/crashwny/gardisto/archive/main.zip
+unzip main.zip
+mv gardisto-main/ /home/gardisto/
+cd /home/gardisto/gardisto-main/
+chown -R gardisto:gardisto /home/gardisto/gardisto-main/
 
 read -p "Enter Gardisto's FQDN or IP address: " gardfqdn
 
 sed "s/SERVER_FQDN=/SERVER_FQDN=$gardfqdn/" gardisto.conf > /var/gardisto/gardisto.conf
 
 sed -i "s/gardisto.server.ip/$gardfqdn/" satellite-setup.sh
+
+runuser -l gardisto -c 'pip3 install --user -e /home/gardisto/gardisto-main/'
