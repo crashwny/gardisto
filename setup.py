6a2a758874e49a488cf48a320dbff0e40c4ce10c
Patrick M. Covert <pmcovert@buffaloist.com>
2021-02-28 20:26:34 -0500

    minor update to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import find_packages, setup
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
 setup(
     name='Gardisto',
     version='0.2.2',
     author='Patrick M. Covert',
     author_email='pmcovert@buffaloist.com',
-    description='A basic systems monitor utility',
-    long_description=long_description,
+    description='A basic linux systems monitor utility',
+    long_description=open('README.md').read()
     long_description_content_type='text/markdown',
     url='https://gardisto.org',
     packages=find_packages('src'),
