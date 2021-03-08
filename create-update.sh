ea7db940a25b4d4332ba564c8c56aacc542e3887
Patrick M. Covert <pmcovert@buffaloist.com>
2021-03-07 22:51:26 -0500

    fixing create update script, correcting file locations

--- create-update.sh
+++ create-update.sh
@@ -2,7 +2,7 @@
 # create an update package for satellites 0.2.2 to 0.2.3
 
 mkdir -p /tmp/update-package/collectors
-cp /var/gardisto/collectors/gar* /tmp/update-package/collectors/
+cp ~/gardisto/collectors/* /tmp/update-package/collectors/
 cp /var/gardisto/gardisto.conf
 
 tar -czf /var/gardisto/update-package.tar.gz /tmp/update-package
