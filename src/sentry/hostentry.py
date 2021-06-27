02abfa5650e24acc647c86af6b841b68cb9385a3
Patrick Covert <pmcovert@buffaloist.com>
2021-06-26 22:57:09 -0400

    bug - return data

--- src/sentry/hostentry.py
+++ src/sentry/hostentry.py
@@ -55,6 +55,7 @@ def script():
 
 def jsonparse(jdata):
     entrydata = (jdata["hostname"], jdata["ip"], jdata["fqdn"], "0", "0", "0")
-
+    return entrydata
+    
 if __name__ == "__main__":
     start()
